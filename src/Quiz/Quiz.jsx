0d196be00f8bec5aaf91e8e1c4639c3a10e5027b
Brendan Del Rosario <brendandelrosario@gmail.com>
2016-06-29 23:37:03 -0700

    added count component and fixed question component

--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -1,15 +1,33 @@
 const React = require('react');
 const Question = require('./Question.jsx');
-const Answer = require('./Answer.jsx');
+// const Answer = require('./Answer.jsx');
 const Timer = require('./Timer.jsx');
+const Count = require('./Count.jsx');
+// expecting an object that contains 3 arrays, each containing all
+// the question data needed. (question,answers, correct answer, reasons)
+// expecting prop to be named dailyQuestions
 
 const Quiz = React.createClass({
+  getInitialState() {
+    return ({
+      dailyQuestions: {
+        1: ['1'],
+        2: ['2'],
+        3: ['3'],
+      },
+    });
+  },
   render() {
     return (
       <div>
-        <Question />
-        <Answer />
+        <Count
+          numberOfQuesiton={Object.keys(this.state.dailyQuestions).length}
+        />
         <Timer />
+        <Question
+          question={this.state.dailyQuestions[1]}
+        />
+        <submit />
       </div>
      );
   },
