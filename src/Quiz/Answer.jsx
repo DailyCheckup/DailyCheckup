c4de62417b986d4219d467d896374c31b02dc225
Brendan Del Rosario <brendandelrosario@gmail.com>
2016-06-29 22:21:38 -0700

    added quiz folder that contains quiz,question,answer,timer components

---
+++ src/Quiz/Answer.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const Answer = React.createClass({
+  render() {
+    return (
+      <div>
+        answer
+      </div>
+     );
+  },
+});
+
+module.exports = Answer;
