ae100168f29f21e1b7e7de567adff681b9de0ceb
Sandra Winkler <sandra.winkler.h@gmail.com>
2016-07-08 13:40:17 -0700

    began login and quiz component testing

--- test/tests/Login-spec.js
+++ test/tests/Login-spec.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
-import Login from './../../src/Components/Login.jsx';
+import Login from './../../src/Components/login.jsx';
 
-const dummyResults1 = JSON.stringify(
+const dummyResults = JSON.stringify(
   {
     results: {
       isAdmin: true,
       changedPassword: true,
       email: 'sandra@hi.com',
-      dailyQuestions: ['question1', 'question2', 'question3'],
+      dailyQuestions: [],
     },
   }
 );
 
-describe('Testing login component', () => {
-  const wrapper = mount(<Login />);
+describe('Login Component', () => {
+
+  const wrapper = shallow(<Login />);
+
+  it('contains a form component', () => {
+    expect(wrapper.find('form')).to.have.length(1);
+  });
+
+  it('contains two inputs', () => {
+    expect(wrapper.find('input')).to.have.length(2);
+  });
 
   it('contains an email input', () => {
-    expect(shallow(<Login />).find(<input type="email" />)).to.equal(true);
+    expect(wrapper.find('input [type="email"]')).to.have.length(1);
   });
 
   it('contains a password input', () => {
-    expect(shallow(<Login />).find(<input type="password" />)).to.equal(true);
+    expect(wrapper.find('input [type="password"]')).to.have.length(1);
   });
 
+  it('contains a sign in button', () => {
+    expect(wrapper.find('button')).to.have.length(1);
+  });
 });
--- test/tests/Quiz-spec.js
+++ test/tests/Quiz-spec.js
@@ -2,21 +2,54 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import Quiz from './../../src/Components/Quiz/Quiz.jsx';
-// import Question from './../../src/Components/Quiz/Question.jsx';
-// import Answer from './../../src/Components/Quiz/Answer.jsx';
+import Answers from './../../src/Components/Quiz/Answers.jsx';
+
+const testQuestions = [{
+a:"A) severe joint pain",
+answer:"A",
+b:"B) marked joint space narrowing on radiologic studies",
+c:"C) destruction and loss of motion of the contralateral joint",
+chosen:true,
+createdAt:"2016-07-07T05:53:11.776Z",
+d:"D) an acutely infected joint",
+e:"null",
+genre:"GI",
+id:139,
+question:"139. A 70-year-old retired farmer presents with an angulated right knee and a painful hip. He asksyou about the possibility of having knee replacement surgery, although he is not eager to do so.You would advise him that the major indication for knee replacement is",
+questionid:139,
+reason:"39The major indication for joint replacement is severe joint pain. Loss of joint function and radiographicevidence of severe destruction of the joint may also be considered in the decision. The appearance of thejoint and the status of the contralateral joint may be minor considerations. Surgical insertion of a foreignbody into an infected joint is contraindicated. Ref: Zhang W, Moskowitz RW, Nuki G, et al: OARSI recommendations for the management of hip and knee osteoarthritis, PartII: OARSI evidence-based, expert consensus guidelines. Osteoarthritis Cartilage 2008;16(2):137-162. ",
+updatedAt:"2016-07-08T01:32:24.410Z"},
+{a:"A) Emollients",
+answer:"D",
+b:"B) Pimecrolimus (Elidel)",
+c:"C) Mupirocin (Bactroban)",
+chosen:true,
+createdAt:"2016-07-07T05:53:11.779Z",
+d:"D) Corticosteroids",
+e:"E) Antihistamines",
+genre:"Endocrine",
+id:177,
+question:"178. A mother brings her 10-year-old son to your office because he has recently experienced aflare-up of atopic dermatitis, including increased pruritus. Physical findings include increasederythema of the involved skin on the flexural surfaces of his arms and legs, with weepingeruptions located within areas of lichenification.Which one of the following topical treatments for managing this episode is supported by the bestavailable evidence?",
+questionid:178,
+reason:"78Emollients are a mainstay of chronic therapy for atopic dermatitis (SOR C), but topical corticosteroids arethe first-line treatment for flare-ups (SOR A). Calcineurin inhibitors such as pimecrolimus are asecond-line treatment for moderate to severe atopic dermatitis (SOR A). Antibiotics are not useful inreducing flare-ups of atopic dermatitis unless there is clear evidence of a secondary infection (SOR A).Neither topical nor oral antihistamines are recommended for routine treatment of atopic dermatitis becausethey are not effective in treating the associated pruritus. Ref: Berke R, Singh A, Guralnick M: Atopic dermatitis: An overview. Am Fam Physician 2012;86(1):35-42. ",
+updatedAt:"2016-07-08T01:32:24.410Z"}
+];
 
 describe('Testing Quiz Component', () => {
-  const wrapper = mount(<Quiz />);
+  const testState = {
+    userEmail: '',
+    changedPW: '',
+    isAdmin: '',
+    dailyQuestions: testQuestions,
+    takenQuiz: false,
+    quizAvailability: false,
+  };
 
-  it('should have a Question Component', () => {
-    expect(wrapper.find('Question').length).to.equal(1);
+  const wrapper = mount(<Quiz getState={testState} />);
+
+  it('should have an Answers Component', () => {
+    //console.log(wrapper.find('Answers'), 'download atom pls')
+    expect(wrapper.find(Answers)).to.have.length(0);
   });
 
-  it('should have an Answer Component', () => {
-    expect(wrapper.find('Answer').length).to.equal(1);
-  });
-
-  it('should have a Timer Component', () => {
-    expect(wrapper.find('Timer').length).to.equal(1);
-  });
 });
