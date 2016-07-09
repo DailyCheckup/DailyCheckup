2e8b2c12cb3d0eaf7d3483c122f51a941b391be8
Brendan Del Rosario <brendandelrosario@gmail.com>
2016-07-08 21:30:19 -0700

    added coveralls support

--- test/tests/server-test.js
+++ test/tests/server-test.js
@@ -3,8 +3,8 @@ var test = require('tape');
 var request = require('supertest');
 var app = require('./../../server/server.js');
 
-test('Correct status code BLAH', function (t) {
-  var userInput = { emailAddress: 'bignoob', password: 'testing' };
+test('Correct User Login', function (t) {
+  var userInput = { emailAddress: 'demo', password: 'testing' };
   t.plan(1);
   request(app)
     .post('/login')
@@ -12,9 +12,7 @@ test('Correct status code BLAH', function (t) {
     .expect(200)
     .expect('Content-Type', /json/)
     .end(function (err, res) {
-      console.log(res.body, 'this is request app stuff');
       t.same(res.status, 200, 'correct status code was sent');
-      // t.same(res.body, {'email':'test@email.com','password':'admin'}, 'the correct body was posted to /login');
       t.end();
       app.destroy();
     });
