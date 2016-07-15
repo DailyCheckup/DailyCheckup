d56fe6eabf29ad9c608afa34fc8fc3f143b00efa
Sandra Winkler <sandra.winkler.h@gmail.com>
2016-07-15 11:52:09 -0700

    updated group data table to include n_option

--- server/Questions/groupDataModel.js
+++ server/Questions/groupDataModel.js
@@ -2,24 +2,53 @@ const Sequelize = require('sequelize');
 const DB = require('../DB/Database.js');
 
 const groupData = DB.define('groupdata', {
-  questionid: Sequelize.INTEGER,
-  date: Sequelize.DATEONLY,
-  question: Sequelize.TEXT,
-  answer: Sequelize.TEXT,
-  reason: Sequelize.TEXT,
-  num_of_people_correct: Sequelize.INTEGER,
-  num_of_people_incorrect: Sequelize.INTEGER,
-  genre: Sequelize.TEXT,
-  a_count: Sequelize.INTEGER,
-  b_count: Sequelize.INTEGER,
-  c_count: Sequelize.INTEGER,
-  d_count: Sequelize.INTEGER,
-  e_count: Sequelize.INTEGER,
-  a_option: Sequelize.TEXT,
-  b_option: Sequelize.TEXT,
-  c_option: Sequelize.TEXT,
-  d_option: Sequelize.TEXT,
-  e_option: Sequelize.TEXT,
+ questionid: Sequelize.INTEGER,
+ date: Sequelize.DATEONLY,
+ question: Sequelize.TEXT,
+ answer: Sequelize.TEXT,
+ reason: Sequelize.TEXT,
+ num_of_people_correct: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ num_of_people_incorrect: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ num_of_people_total: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ genre: Sequelize.TEXT,
+ a_count: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ b_count: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ c_count: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ d_count: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ e_count: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ n_count: {
+   type: Sequelize.INTEGER,
+   defaultValue: 0,
+ },
+ a_option: Sequelize.TEXT,
+ b_option: Sequelize.TEXT,
+ c_option: Sequelize.TEXT,
+ d_option: Sequelize.TEXT,
+ e_option: Sequelize.TEXT,
 });
-
+//groupData.sync();
 module.exports = groupData;
