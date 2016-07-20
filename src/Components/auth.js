e6e2483b03ecb0405aa3b96545b110caa3574e55
anonymous <swimkitch92@gmail.com>
2016-07-20 11:53:10 -0700

    added refresh functionality with jwt and only admins can enter director routes. While logged in, will redirect to appropriate home page based off jwt

--- src/Components/auth.js
+++ src/Components/auth.js
@@ -1,4 +1,4 @@
-
+const jwtDecode = require('jwt-decode');
 
 function loggedIn() {
   return !!localStorage.DailyCheckupToken;
@@ -21,6 +21,25 @@ module.exports = {
         pathname: '/',
         state: { nextPathname: nextState.location.pathname },
       });
+    } else {
+      const token = jwtDecode(localStorage.DailyCheckupToken);
+      if (!token.isAdmin) {
+        replace({
+          pathname: '/',
+          state: { nextPathname: nextState.location.pathname },
+        });
+      }
     }
   },
+  goHome(nextState, replace) {
+    if (loggedIn()) {
+      const token = jwtDecode(localStorage.DailyCheckupToken);
+      if (token.isAdmin) {
+        replace('/director');
+      } else {
+        replace('/resident');
+      }
+    }
+  },
+
 };
