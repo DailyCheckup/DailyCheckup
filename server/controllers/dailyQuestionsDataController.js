fe158d99e2a750962747ce7762c37d216020879f
Sandra Winkler <sandra.winkler.h@gmail.com>
2016-07-28 16:36:53 -0700

    added additional comments and changed 'N' column label to 'N/A' label

--- server/controllers/dailyQuestionsDataController.js
+++ server/controllers/dailyQuestionsDataController.js
@@ -7,42 +7,52 @@ const todaysDate = moment(dateTimeZoneAdjusted).format('YYYY-MM-DD');
 const dailyResults = {
 
   dailyQuestionsResults(req, res, next) {
+    // Find group data rows with today's date
     groupData.findAll({
       where: {
         date: todaysDate,
       }
     }).then((dayResults) => {
-      // daily results is an array of objects, grab the dataValues
+      // Daily results is an array of question objects, grab the dataValues
       let results = dayResults.map(function (result) {
         return result.dataValues;
       });
-      // Create column chart arrays
+      // Create a column chart array for each question and store it in a columnChartArray property
       results.forEach(function (result) {
         result.columnChartArray = dailyResults.buildColumnChartData(result);
       });
-      // Need to add in new condition for checking if no quizzes have been taken
+      // Check if no quizzes have been taken during the day
       if (results[0].num_of_people_total === 0) {
         results = 'No quizzes have been taken today';
       }
+      // Create a results obj to store the gathered data
       req.results = {};
+      // Todays results is an array of question objects
       req.results.todaysResults = results;
       next();
     });
   },
 
   buildColumnChartData(data) {
+    // Data is one question obj
     const columnChartData = [];
-    // Need to add {role: 'style'} for colors to column axis
-    const columnAxes = ['Selection', 'Count', {role: 'style'}];
+    const columnAxes = ['Selection', 'Count', { role: 'style' }];
     columnChartData.push(columnAxes);
     const selections = ['A', 'B', 'C', 'D', 'E', 'N'];
     selections.forEach(function (select) {
-      // Works only for traditional charts, not material charts
+      // Column colors only work for traditional charts, not material charts
+      let selectLetter = select;
       let color = 'color: #536DFE';
       if (data.answer === select) {
         color = 'color: #66BB6A';
       }
-      columnChartData.push([select, data[select.toLowerCase() + '_count'], color]);
+      if (selectLetter === 'N') {
+        selectLetter = 'N/A';
+      }
+      // Add a row to the column chart with the letter,
+      // number of people that chose that letter,
+      // and the color of the column
+      columnChartData.push([selectLetter, data[`${select.toLowerCase()}_count`], color]);
     });
     return columnChartData;
   },
