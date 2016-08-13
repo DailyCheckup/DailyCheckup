f2b29f3dc5cad30e6d423b5c06861e32a982112b
Sandra Winkler <sandra.winkler.h@gmail.com>
2016-08-12 19:29:22 -0700

    set up foundation for redux. Added store, reducers folder, provider component  and npm installed redux and react-redux

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './../store';
 import Routes from './reactRoutes';
 require('../Styles/styles.scss');
+
+// Uncomment when reducers are up and running
+// ReactDOM.render(<Provider store={store}> {Routes} </Provider>, document.getElementById('content'));
 
 ReactDOM.render(<Routes />, document.getElementById('content'));
