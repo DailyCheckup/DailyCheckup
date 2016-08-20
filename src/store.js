408ac6c8c0b6d2719611b86dfc470a401212154a
Sandra Winkler <sandra.winkler.h@gmail.com>
2016-08-20 16:58:24 -0700

    fixed dom render bug and cleaned up store code

--- src/store.js
+++ src/store.js
@@ -1,28 +1,5 @@
-// moved login reducer here for now because kept getting error that 'reducers is not a function'
 import { createStore } from 'redux';
 import reducers from './Reducers/index';
-// import { loginReducer } from './Reducers/loginReducer';
-
-// import * as types from './actions/actionTypes';
-
-// const initialState = {
-//   userEmail: '',
-//   changedPW: '',
-//   isAdmin: '',
-//   dailyQuestions: [],
-//   takenQuiz: false,
-//   quizAvailability: false,
-//   firstName: '',
-//   loggedIn: false,
-// };
-
-// function loginReducer(state = initialState, action) {
-//   if (action.type === types.LOGIN_SUCCESS) {
-//     return Object.assign({}, state, action.userInfo);
-//   }
-//   return state;
-// }
 
 const store = createStore(reducers);
-// const store = createStore(loginReducer);
 export default store;
